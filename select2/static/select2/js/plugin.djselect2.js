b749470a6d7a6d30510e7d2ee92a0f1e11b031b9
smoothify <dev@smoothify.com>
2014-02-06 13:20:24 +0000

    Remove boilerplate copyright.

--- select2/static/select2/js/plugin.djselect2.js
+++ select2/static/select2/js/plugin.djselect2.js
@@ -1,11 +1,3 @@
-/*!
- * jQuery lightweight plugin boilerplate
- * Original author: @ajpiano
- * Further changes, comments: @addyosmani
- * Licensed under the MIT license
- */
-
-
 // the semi-colon before the function invocation is a safety
 // net against concatenated scripts and/or other plugins
 // that are not closed properly.
