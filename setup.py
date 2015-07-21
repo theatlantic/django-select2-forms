461f78cb060a624afd781b38a85eb289ccbf16ca
Frankie Dintino <fdintino@theatlantic.com>
2015-07-21 18:38:42 -0400

    Bump to v1.1.23

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except IOError:
 
 setup(
     name='django-select2-forms',
-    version='1.1.22',
+    version='1.1.23',
     description='Django form fields using the Select2 jQuery plugin',
     author='Frankie Dintino',
     author_email='fdintino@theatlantic.com',
