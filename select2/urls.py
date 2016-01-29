a9d4fdba78c33652f7cc3985d9e1cadfd707406d
Frankie Dintino <fdintino@theatlantic.com>
2016-01-29 16:35:08 -0500

    Fix DeprecationWarning by changing django.conf.urls.patterns() to list

--- select2/urls.py
+++ select2/urls.py
@@ -1,12 +1,14 @@
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
+
+import select2.views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^fetch_items/(?P<app_label>[^\/]+)/(?P<model_name>[^\/]+)/(?P<field_name>[^\/]+)/$',
-        'select2.views.fetch_items', name='select2_fetch_items'),
+        select2.views.fetch_items, name='select2_fetch_items'),
     url(r'^init_selection/(?P<app_label>[^\/]+)/(?P<model_name>[^\/]+)/(?P<field_name>[^\/]+)/$',
-        'select2.views.init_selection', name='select2_init_selection'),
-)
+        select2.views.init_selection, name='select2_init_selection'),
+]
