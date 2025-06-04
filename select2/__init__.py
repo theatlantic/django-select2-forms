d27f01b32539a46ec345408f65c824e93f175581
Matheus Schreiber <mt.schreiber01@gmail.com>
2025-06-04 13:02:46 -0300

    :bug: fix setup warning

--- select2/__init__.py
+++ select2/__init__.py
@@ -1,6 +1,11 @@
-import pkg_resources
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # For Python <3.8, fallback to importlib_metadata backport
+    from importlib_metadata import version, PackageNotFoundError
 
 try:
-    __version__ = pkg_resources.get_distribution('django-select2-forms').version
-except pkg_resources.DistributionNotFound:
+    __version__ = version('django-select2-forms')
+except PackageNotFoundError:
     __version__ = None
+
